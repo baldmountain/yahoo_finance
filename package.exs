9e9ffe57a01d35bf6e3065a736149fc2e4e23ead
Geoffrey Clements <baldmountain@gmail.com>
2013-06-13 22:16:16 -0400

    Add an expm.co package file

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "yahoo_finance", description: "An API for accessing Yahoo Finance data",
+                 version: "0.1.0", keywords: ["yahoo", "finance", "stock", "quote"], 
+                 maintainers: [[name: "Geoffrey Clements", 
+                                email: "baldmountain@gmail.com"]],
+                 repositories: [[github: "baldmountain/yahoo_finance"]])
+    
